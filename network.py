6be70a0746375b775fbd2f2a30233c160b6f2bc2
louisf412 <126870649+louisf412@users.noreply.github.com>
2023-03-03 21:36:39 +1100

    network.py

---
+++ network.py
@@ -0,0 +1,28 @@
+import socket
+import pickle
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "118.138.196.207" # replace with your ipv4 address to test the code. also replace in server.py
+        self.port = 5050
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048*2))
+        except socket.error as e:
+            print(e)
