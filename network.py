e856fb3cd8390d019855d9de5fd0ad91ae84b651
louisf412 <126870649+louisf412@users.noreply.github.com>
2023-03-03 23:02:44 +1100

    Update network.py

--- network.py
+++ network.py
@@ -16,13 +16,13 @@ class Network:
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(1024*8))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return pickle.loads(self.client.recv(2048*2))
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(1024*8))
         except socket.error as e:
             print(e)
