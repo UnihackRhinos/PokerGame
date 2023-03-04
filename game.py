06708c98c10eeb2381fe1df88fbd89803ac8cb9f
macka275 <126256109+macka275@users.noreply.github.com>
2023-03-04 19:58:27 +1100

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import random
 import time
 
+
 class Card:
     def __init__(self, suit, value):
         self.suit = suit
@@ -23,46 +24,46 @@ class Deck:
             for j in Deck.values:
                 self.cards.append(Card(i, j).show())
 
+
 deck = Deck().cards
 runout = []
 p1_hand = []
 p2_hand = []
+
+
 def deal():
     for i in range(2):
-        the_int = random.randint(0, len(deck)-1)
+        the_int = random.randint(0, len(deck) - 1)
         rand_card = deck[the_int]
         p1_hand.append(rand_card)
         deck.pop(the_int)
     for i in range(2):
-        the_int = random.randint(0, len(deck)-1)
+        the_int = random.randint(0, len(deck) - 1)
         rand_card = deck[the_int]
         p2_hand.append(rand_card)
         deck.pop(the_int)
 
+
 def flop():
     for i in range(3):
-        the_int = random.randint(0, len(deck)-1)
+        the_int = random.randint(0, len(deck) - 1)
         rand_card = deck[the_int]
         runout.append(rand_card)
         deck.pop(the_int)
 
+
 def turn():
-    the_int = random.randint(0, len(deck)-1)
+    the_int = random.randint(0, len(deck) - 1)
     rand_card = deck[the_int]
     runout.append(rand_card)
     deck.pop(the_int)
+
 
 def river():
-    the_int = random.randint(0, len(deck)-1)
+    the_int = random.randint(0, len(deck) - 1)
     rand_card = deck[the_int]
     runout.append(rand_card)
     deck.pop(the_int)
-
-p1_stack = 0
-p2_stack = 0
-num_of_turns = 0
-def betting():
-
 
 
 def play_round():
@@ -72,7 +73,7 @@ def play_round():
     print(p1_hand)
     print("Player 2's hand")
     print(p2_hand)
-    time.sleep(3)
+    betting()
     flop()
     print('')
     print('The Flop')
@@ -88,10 +89,80 @@ def play_round():
     print('The River')
     print(runout)
 
+
 class Player():
     def __init__(self, name, money=500):
         self.name = name
         self.money = money
+
+
+def int_input(prompt="", restricted_to=None):
+    while True:
+        player_input = input(prompt)
+        try:
+            int_player_input = int(player_input)
+        except ValueError:
+            continue
+        if restricted_to is None:
+            break
+        elif int_player_input in restricted_to:
+            break
+    return int_player_input
+
+
+stack = 200
+num_of_turns = 0
+bigblind = 20
+smallblind = 10
+position = 0
+pot = 0
+other_stack = 0
+round_over = False
+win = False
+
+
+def betting():
+    global pot
+    global stack
+    committed = 0
+    # preflop
+    if position == 0:
+        pot += bigblind
+        stack -= bigblind
+        committed += bigblind
+    elif position == 1:
+        pot += smallblind
+        stack -= smallblind
+        committed += smallblind
+    if pot / 2 != committed:  #finds out if player is able to call
+        choice = int_input("Call: 1, Raise: 2, Fold: 3. Please select option: ", [1, 2, 3])
+        if choice == 1:
+            committed += pot - committed
+            stack -= committed
+            pot += committed
+        elif choice == 2:
+            raise_amount = int_input("How much would you like to raise by? ", list(range(bigblind, stack + 1)))
+            pot += raise_amount
+            stack -= raise_amount
+            committed += raise_amount
+        elif choice == 3:
+            round_over = True
+
+    if pot / 2 == committed:  #finds out if player is able to check
+        choice = int_input("Check: 1, Raise: 2, Fold: 3. Please select option: ", [1, 2, 3])
+        if choice == 1:
+            pass
+        elif choice == 2:
+            raise_amount = int_input("How much would you like to raise by? ", list(range(bigblind, stack + 1)))
+            pot += raise_amount
+            stack -= raise_amount
+            committed += raise_amount
+        elif choice == 3:
+            round_over = True
+
+
+
+betting()
 
 
 
