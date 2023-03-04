4a546ababe5b13de5dd37c37aae492a93e5a940d
macka275 <126256109+macka275@users.noreply.github.com>
2023-03-04 23:07:14 +1100

    Update game.py

--- game.py
+++ game.py
@@ -25,77 +25,6 @@ class Deck:
                 self.cards.append(Card(i, j).show())
 
 
-deck = Deck().cards
-runout = []
-p1_hand = []
-p2_hand = []
-
-
-def deal():
-    for i in range(2):
-        the_int = random.randint(0, len(deck) - 1)
-        rand_card = deck[the_int]
-        p1_hand.append(rand_card)
-        deck.pop(the_int)
-    for i in range(2):
-        the_int = random.randint(0, len(deck) - 1)
-        rand_card = deck[the_int]
-        p2_hand.append(rand_card)
-        deck.pop(the_int)
-
-
-def flop():
-    for i in range(3):
-        the_int = random.randint(0, len(deck) - 1)
-        rand_card = deck[the_int]
-        runout.append(rand_card)
-        deck.pop(the_int)
-
-
-def turn():
-    the_int = random.randint(0, len(deck) - 1)
-    rand_card = deck[the_int]
-    runout.append(rand_card)
-    deck.pop(the_int)
-
-
-def river():
-    the_int = random.randint(0, len(deck) - 1)
-    rand_card = deck[the_int]
-    runout.append(rand_card)
-    deck.pop(the_int)
-
-
-def play_round():
-    input("Are you ready to play poker? ")
-    deal()
-    print("Player 1's hand")
-    print(p1_hand)
-    print("Player 2's hand")
-    print(p2_hand)
-    betting()
-    flop()
-    print('')
-    print('The Flop')
-    print(runout)
-    time.sleep(3)
-    turn()
-    print('')
-    print('The Turn')
-    print(runout)
-    time.sleep(3)
-    river()
-    print('')
-    print('The River')
-    print(runout)
-
-
-class Player():
-    def __init__(self, name, money=500):
-        self.name = name
-        self.money = money
-
-
 def int_input(prompt="", restricted_to=None):
     while True:
         player_input = input(prompt)
@@ -110,59 +39,117 @@ def int_input(prompt="", restricted_to=None):
     return int_player_input
 
 
-stack = 200
-num_of_turns = 0
-bigblind = 20
-smallblind = 10
-position = 0
-pot = 0
-other_stack = 0
-round_over = False
-win = False
+class Game:
+    bigblind = 20
+    smallblind = 10
+
+    def __init__(self, player):
+        self.player = player
+        self.deck = Deck().cards
+        self.runout = []
+        self.hand = [[], []]
+        self.stack = [200, 200]
+        self.num_of_actions = [0, 0]
+        self.position = [0, 1]
+        self.pot = 0
+        self.hand_over = False
+        self.committed = 0
+        self.owed = [0, 0]
+        self.betting_round = 0
+        self.player_has_folded = False
+
+    def deal(self):
+        for i in range(2):
+            the_int = random.randint(0, len(self.deck) - 1)
+            rand_card = self.deck[the_int]
+            self.hand[0].append(rand_card)
+            self.deck.pop(the_int)
+        for i in range(2):
+            the_int = random.randint(0, len(self.deck) - 1)
+            rand_card = self.deck[the_int]
+            self.hand[1].append(rand_card)
+            self.deck.pop(the_int)
+
+    def flop(self):
+        for i in range(3):
+            the_int = random.randint(0, len(self.deck) - 1)
+            rand_card = self.deck[the_int]
+            self.runout.append(rand_card)
+            self.deck.pop(the_int)
+
+    def turn(self):
+        the_int = random.randint(0, len(self.deck) - 1)
+        rand_card = self.deck[the_int]
+        self.runout.append(rand_card)
+        self.deck.pop(the_int)
+
+    def river(self):
+        the_int = random.randint(0, len(self.deck) - 1)
+        rand_card = self.deck[the_int]
+        self.runout.append(rand_card)
+        self.deck.pop(the_int)
+
+    def blinds(self):
+        if self.position[self.player] == 0:  # not dealer and bigblind
+            self.pot += self.bigblind
+            self.stack[self.player] -= self.bigblind
+            self.committed += self.bigblind
+        elif self.position[self.player] == 1:  # is dealer and smallblind
+            self.pot += self.smallblind
+            self.stack[self.player] -= self.smallblind
+            self.committed += self.smallblind
+            self.owed[self.player] = 10
+
+    def betting(self, choice, raise_amount = bigblind):
+        if self.owed[self.player]:  # finds out if player is able to call
+            if choice == 1:     #if player chooses to call
+                self.committed += self.owed[self.player]
+                self.stack[self.player] -= self.owed[self.player]
+                self.pot += self.owed[self.player]
+                self.owed[self.player] = 0
+                self.num_of_actions[self.player] += 1
+            elif choice == 2:   #if player chooses to raise
+                self.committed += raise_amount + self.owed[self.player]
+                self.stack[self.player] -= (raise_amount + self.owed[self.player])
+                self.pot += raise_amount + self.owed[self.player]
+                self.owed[self.player] = 0
+                self.owed[(self.player + 1) % 2] = raise_amount
+                self.num_of_actions[self.player] += 1
+            elif choice == 3:   #if player chooses to fold
+                self.hand_over = True
+                self.player_has_folded = True
+
+        elif self.owed[self.player] == 0 and self.num_of_actions[self.player] >= 1:  # finds out if player is able to check
+            if choice == 1:
+                self.num_of_actions[self.player] += 1
+
+            elif choice == 2:  # if player chooses to raise
+                self.committed += raise_amount + self.owed[self.player]
+                self.stack[self.player] -= (raise_amount + self.owed[self.player])
+                self.pot += raise_amount + self.owed[self.player]
+                self.owed[self.player] = 0
+                self.owed[(self.player + 1) % 2] = raise_amount
+                self.num_of_actions[self.player] += 1
+            elif choice == 3:  # if player chooses to fold
+                self.hand_over = True
+                self.player_has_folded = True
+
+        else:
+            self.betting_round += 1
 
 
-def betting():
-    global pot
-    global stack
-    committed = 0
-    # preflop
-    if position == 0:
-        pot += bigblind
-        stack -= bigblind
-        committed += bigblind
-    elif position == 1:
-        pot += smallblind
-        stack -= smallblind
-        committed += smallblind
-    if pot / 2 != committed:  #finds out if player is able to call
-        choice = int_input("Call: 1, Raise: 2, Fold: 3. Please select option: ", [1, 2, 3])
-        if choice == 1:
-            committed += pot - committed
-            stack -= committed
-            pot += committed
-        elif choice == 2:
-            raise_amount = int_input("How much would you like to raise by? ", list(range(bigblind, stack + 1)))
-            pot += raise_amount
-            stack -= raise_amount
-            committed += raise_amount
-        elif choice == 3:
-            round_over = True
+    def play_game(self):
+        while not self.hand_over:
+            self.blinds()
+            self.deal()
+            self.betting()
+            self.flop()
+            self.betting()
+            self.turn()
+            self.betting()
+            self.river()
+            self.betting()
 
-    if pot / 2 == committed:  #finds out if player is able to check
-        choice = int_input("Check: 1, Raise: 2, Fold: 3. Please select option: ", [1, 2, 3])
-        if choice == 1:
-            pass
-        elif choice == 2:
-            raise_amount = int_input("How much would you like to raise by? ", list(range(bigblind, stack + 1)))
-            pot += raise_amount
-            stack -= raise_amount
-            committed += raise_amount
-        elif choice == 3:
-            round_over = True
-
-
-
-betting()
-
-
+game1 = Game(1).play_game
+game1()
 
