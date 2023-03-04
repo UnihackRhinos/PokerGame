3bf3bbc52d1f9689924b3398eaa0069a7f49ed92
codygmonash <126865831+codygmonash@users.noreply.github.com>
2023-03-03 20:51:47 -0500

    Update WinSearch.py

--- WinSearch.py
+++ WinSearch.py
@@ -5,7 +5,7 @@ class Deck():
     hand_1 = []
     hand_2 = []
     scored = []
-    score = (0, 0)
+    score = []
 
     def SearchWin():
         #Get suit numbers
@@ -17,20 +17,20 @@ class Deck():
         for i in range(suits):
             #Flush
             if suits[i] >= 5:       #if more than 5 cards of 1 suit
-                for card in hand_1:     #Remove cards not of flush suit and add values to tiebreak score
+                for card in hand_1:     #Remove cards not of flush suit
                     if card.suit != suits[1]:
-                        score[1] += card.value
                         hand_1.remove(card)
                 hand_1.sort(key=Card.value)     #Sort cards of similar suit
-                for i in range(hand_1.count - 1):       
-                    #Flush (not straight)   
+                for i in range(hand_1.count - 1):  
+                    score.append = hand_count[i].value  #Highest card in flush     
+                    #Flush (not straight)   (6, highest card)
                     if Math.abs(hand_1[i].num - hand_1[i+1]) != 1:      #if  not consecutive numbers --> flush
-                        score[0] = 6
+                        score.insert(0, 6)
                         return
-                #Straight Flush
-                score[0] = 9
+                #Straight Flush (9, highest card)
+                score(0, 9)
                 return
-            hand_1.sort(key=Card.value)
+            hand_1.sort(reverse=true, key=Card.value)
             
             #
             #Dictionary<int number, int quantity>
@@ -45,11 +45,65 @@ class Deck():
             for num in repitions.keys:
                 if repition[num] > most:
                     most = num
-            if(most == 4):
+
+            #Four of a kind (8, number of 4, next highest card)
+            if repititions[most] == 4:
+                score.append(8)
+                score.append(most)
+                for card in hand_1:
+                    if card.value != most:
+                        score.append(card.value)
+            
+            if repitions[most] == 3:
+                #3 of a kind (4, number of 3, next two highest cards)
+                score.append(4)
+                score.append(most)
+                repitions.pop(most)
+                for card in hand_1:
+                    if card.value != most:
+                        score.append(card.value)
+                        if(score.count == 4):
+                            break
+                #Full House (7, number of 3, number of 2)
+                if(len(repitions) == 1):
+                    score = [7, most, repitions.keys[0]]
+                return
+            if most == 1:
+                for i in range(len(hand_1)-1):
+                    isStraight = True
+                    if Math.abs(hand_1[i].num - hand_1[i+1]) != 1:      #if  not consecutive numbers --> high card
+                        isStraight
+                    if isStriaght:
+                        #Straight   (5, highest card)
+                        score = [5, hand_1[0]]
+                    else:
+                        #High card  (1, cards from high to low)
+                        score = hand_1
+                        score.insert(0, 1)
+            pairs = []
+            for num in repitions.keys:
+                if repitions[num] == 2:
+                    pairs.append(num)
+            
+            #One pair   (2, pair number, next 3 highest cards)
+            if len(pairs) == 1:
+                score = [2, pairs[0]]
+                for card in hand_1:
+                    if card.value != pairs[0]:
+                        score.append(card.value)
+                        if len(score) == 5:
+                            return
+
+            #Two pair   (3, pair numbers, next highest card)
+            if len(pairs) == 1:
+                score = [2, pairs[0]]
+                for card in hand_1:
+                    if card.value != pairs[0] and card.value != pairs[1]:
+                        score.append(card.value)
+                        return
 
             
 
             
-
 
 Deck()
