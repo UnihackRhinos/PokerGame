1dc13e96817b6d25d5f374d078794e6264477312
louisf412 <126870649+louisf412@users.noreply.github.com>
2023-03-05 15:45:23 +1100

    Update server.py

--- server.py
+++ server.py
@@ -2,8 +2,9 @@ import socket
 from _thread import *
 import pickle
 from game import Game
+from time import sleep
 
-server = "118.138.91.21" # replace this with your ipv4 address to test, also replace in network.py
+server = "118.138.23.2" # replace this with your ipv4 address to test, also replace in network.py
 port = 5050
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,14 +23,32 @@ print("Waiting for a connection, Server Started")
 def threaded_client(conn, player):
     conn.send(pickle.dumps(player))
     reply = ""
-    while True:
+    while gameData.connected:
         try:
             data = pickle.loads(conn.recv(1024*8))
             if not data:
                 break
             else:
                 if data == "reset":
-                    gameData.reset()
+                    print("resetting")
+                    # gameData.reset()
+                    gameData.betting_round = 0
+                    gameData.allin = [0, 0]
+                    gameData.deck = gameData.Deck().cards
+                    gameData.runout = []
+                    gameData.hand = [[], []]
+                    gameData.handstarted = False
+                    gameData.flopstarted = False
+                    gameData.turnstarted = False
+                    gameData.riverstarted = False
+                    gameData.num_of_actions = [0, 0]
+                    #gameData.position = [(gameData.postion[0] + 1) % 2, (gameData.postion[1] + 1) % 2 ]  # who is in position???
+                    gameData.pot = 0
+                    gameData.hand_over = False
+                    # gameData.committed = 0
+                    # gameData.owed = [0, 0]
+                    print("reset")
+                    #sleep(0.5)
                 elif data == "preflop":
                     if not gameData.handstarted:
                         gameData.handstarted = True # changing value in game object
